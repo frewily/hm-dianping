558381859e1c2c3c49d2247288bf35edc40ebdbb
frewily <2994508531@qq.com>
2026-04-12 18:26:30 +0800

    优化订单创建逻辑，添加用户订单限制（悲观锁），创建代理对象进行事务管理

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,8 @@ import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,7 +35,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         //1 查询优惠券
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
@@ -52,7 +53,29 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             //库存不足
             return Result.fail("库存不足");
         }
-        //5 扣减库存
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {//同一用户加一把锁  intern() 方法返回的是同一个对象，因此两个字符串相等,保证了是同一用户
+            //获取代理对象(事务)
+            //确保事务生效（只有通过代理对象调用方法，事务才会生效）
+            //避免事务没有提交就释放锁的安全问题
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+
+    @NonNull
+    @Transactional//事务要想生效，必须保证方法被代理（拿到方法的代理对象进行事务处理）
+    public Result createVoucherOrder(Long voucherId) {
+        //5 一人只能下单一次
+        //5.1 查询订单
+        boolean exist = query().eq("user_id", UserHolder.getUser().getId())
+                .eq("voucher_id", voucherId).count() > 0;
+        //5.2 判断是否已经下过单
+        if (exist) {
+            return Result.fail("用户已经下过单");
+        }
+        //6 扣减库存
         boolean success = seckillVoucherService
                 .update()                                     // ① 创建 UpdateWrapper 对象
                 .setSql("stock = stock - 1")                  // ② 设置更新的 SQL 片段
@@ -63,18 +86,18 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             //扣减库存失败
             return Result.fail("库存不足");
         }
-        //6 创建订单
+        //7 创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
-        //6.1  订单ID
+        //7.1  订单ID
         Long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        //6.2  用户ID
+        //7.2  用户ID
         Long userId = UserHolder.getUser().getId();
         voucherOrder.setUserId(userId);
-        //6.3  代金券ID
+        //7.3  代金券ID
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
-        //7 返回订单ID
+        //8 返回订单ID
         return Result.ok(orderId);
     }
 }
