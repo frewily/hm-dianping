5e824a24b7f3ec812f4ad9ef0eef8573accb2a12
frewily <2994508531@qq.com>
2026-04-13 18:11:54 +0800

    优化SimpleRedisLock类，添加Lua脚本解锁逻辑以提高锁的安全性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -17,6 +20,12 @@ public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(Long timeoutSec) {
@@ -30,13 +39,22 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
-        // 获取线程标识
-        String threadId =ID_PREFIX + Thread.currentThread().getId();
-        // 获取锁中的标识
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)) {
-            // 释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        //调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+
+//    @Override
+//    public void unLock() {
+//        // 获取线程标识
+//        String threadId =ID_PREFIX + Thread.currentThread().getId();
+//        // 获取锁中的标识
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if (threadId.equals(id)) {
+//            // 释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
---
+++ src/main/resources/unlock.lua
@@ -0,0 +1,8 @@
+-- global redis, KEYS, ARGV
+
+--比较线程标示与锁中的标示是否一致
+if(redis.call('get',KEYS[1]) == ARGV[1])  then
+    -- 释放锁 dey key
+    return redis.call('del', KEYS[1])
+end
+return 0;
