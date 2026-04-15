f789ca48116164b0da58a16e2b4d33a3f94e788c
frewily <2994508531@qq.com>
2026-04-15 20:58:24 +0800

    实现查询博客详情的相关逻辑

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    // 查询热门博客
+    Result queryHotBlog(Integer current);
+
+    // 查询博客详情
+    Result queryBlogById(Long id);
 }
