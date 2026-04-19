e9a365ace19e55ad324f546fb1b022fcd1526f75
frewily <2994508531@qq.com>
2026-04-19 17:54:44 +0800

    清理ShopServiceImpl.java中的未使用代码和注释

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -2,7 +2,6 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
@@ -11,9 +10,7 @@ import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.CacheClient;
-import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisData;
-import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.GeoResult;
 import org.springframework.data.geo.GeoResults;
@@ -26,10 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
@@ -71,51 +65,6 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         return Result.ok(shop);
     }
 
-    //创建线程池
-    //private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
-
-//    public Shop queryWithLogicalExpire(Long id){
-//        String key = CACHE_SHOP_KEY + id;
-//        //1 从redis中查询店铺缓存
-//        String shopJson = stringRedisTemplate.opsForValue().get(key);
-//        //2 判断是否存在
-//        if (StrUtil.isBlank(shopJson)) {
-//            //3 不存在，返回null
-//            return null;
-//        }
-//        //4 命中，需要把json反序列化为对象
-//        RedisData redisData = JSONUtil.toBean(shopJson, RedisData.class);
-//        JSONObject data = (JSONObject) redisData.getData();
-//        Shop shop = JSONUtil.toBean(data, Shop.class);
-//        LocalDateTime expireTime = redisData.getExpireTime();
-//        //5 判断是否过期
-//        if (expireTime.isAfter(LocalDateTime.now())) {
-//            //5.1 未过期，直接返回
-//            return shop;
-//        }
-//        //5.2 已过期，需要缓存重建
-//        //6 缓存重建
-//        //6.1 获取互斥锁
-//        String lockKey = LOCK_SHOP_KEY + id;
-//        boolean islock = tryLock(lockKey);
-//        //6.2 判断是否获取成功
-//        if (islock) {
-//            //6.3 成功，开启独立线程，实现缓存重建
-//            CACHE_REBUILD_EXECUTOR.submit(() -> {
-//                try {
-//                    this.saveShopToRedis(id, 20L);
-//                } catch (Exception e) {
-//                    throw new RuntimeException(e);
-//                } finally {
-//                    //7 释放锁
-//                    unLock(lockKey);
-//                }
-//            });
-//        }
-//        //8 返回过期的商铺信息
-//        return shop;
-//    }
-
     public Shop queryWithMutex(Long id){
         String key = CACHE_SHOP_KEY + id;
         //1 从redis中查询店铺缓存
@@ -132,7 +81,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //4 实现缓存重建
         //4.1获取互斥锁
         String LockKey = LOCK_SHOP_KEY + id;
-        Shop shop = null;
+        Shop shop;
         try {
             boolean isLock = tryLock(LockKey);
             //4.2 判断是否获取成功
@@ -163,35 +112,6 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //8 返回
         return shop;
     }
-
-    /*public Shop queryWithPaaThrough(Long id){
-        String key = CACHE_SHOP_KEY + id;
-        //1 从redis中查询店铺缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(key);
-        //2.1 判断是否存在
-        if (StrUtil.isNotBlank(shopJson)) {
-            //存在，返回
-            return JSONUtil.toBean(shopJson, Shop.class);
-        }
-        //2.2 不存在，判断是否为空（下面shopJson就两种情况：null或空字符串）
-        if (shopJson != null) {//-->shopJson为空字符串,直接返回错误信息，不再查询数据库，避免缓存穿透
-            return null;
-
-        }
-        //3 shopJson为null,不存在，根据id查询数据库
-        Shop shop = getById(id);
-        //4 数据库中不存在，返回404
-        if (shop == null) {
-            //4.1 将空值写入redis
-            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
-            //4.2 返回错误信息
-            return null;
-        }
-        //5 存在，写入redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        //6 返回
-        return shop;
-    }*/
 
     //获取锁
     private boolean tryLock(String key) {
